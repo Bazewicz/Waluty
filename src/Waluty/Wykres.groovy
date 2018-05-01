b65e67150a93f114caecf10513aef967e73c5945
Bazewicz <ja4455@o2.pl>
2018-05-01 10:33:07 +0200

    z wykresem
    Program z wykresem

---
+++ src/Waluty/Wykres.groovy
@@ -0,0 +1,40 @@
+package Waluty
+
+import org.jfree.chart.ChartFactory
+import org.jfree.chart.ChartPanel
+import org.jfree.chart.JFreeChart
+import org.jfree.chart.plot.PlotOrientation
+import org.jfree.data.category.CategoryDataset
+import org.jfree.ui.ApplicationFrame
+
+import java.awt.Dimension
+
+class Wykres extends ApplicationFrame{
+    Wykres(String title, String url) {
+        super(title);
+        String urlString = url
+        String xml = Pobieranie.getXmlFileAsString(urlString)
+        final CategoryDataset dataset = XmlSeparator.createDataset(xml);
+        final JFreeChart chart = createChart(dataset);
+        final ChartPanel chartPanel = new ChartPanel(chart);
+        chartPanel.setPreferredSize(new Dimension(500, 270));
+        setContentPane(chartPanel);
+    }
+    private JFreeChart createChart(final CategoryDataset dataset) {
+
+        final JFreeChart chart = ChartFactory.createBarChart(
+                "Wykres walut",         // chart title
+                "Waluty",               // domain axis label
+                "Wartość",                  // range axis label
+                dataset,                  // data
+                PlotOrientation.VERTICAL, // orientation
+                true,                     // include legend
+                true,                     // tooltips?
+                false                     // URLs?
+        );
+
+
+        return chart;
+
+    }
+}
